e41b58becdd0141baa438ff843cfb567308209c9
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2022-02-21 13:18:29 +0000

    Authorize access routes

--- src/main/java/com/example/spring_security/security/SecurityConfig.java
+++ src/main/java/com/example/spring_security/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.spring_security.filter.ConstumAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().anyRequest().authenticated();
         http.authorizeRequests().anyRequest().permitAll();
         http.addFilter(new ConstumAuthenticationFilter(authenticationManagerBean()));
     }
